3beed36e0d0163eaf124d40284a7142eb78383dd
hansmond93 <hansmond93@gmail.com>
2020-04-22 10:52:12 +0100

    dating App For Publish

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,152 @@
+﻿using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository _repo;
+        private readonly IMapper _mapper;
+        public MessagesController(IMapper mapper, IDatingRepository repo)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet("id", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto message)
+        {
+            var sender = await _repo.GetUser(userId);   //we call this guy so automapper can automatically map our sender details in the messageToReturnDto
+            if (sender.Id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))    //we use the senderId here to verify against the id from the token
+                return Unauthorized();
+
+            message.SenderId = userId;
+
+            var recepient = await _repo.GetUser(message.RecepientId);
+
+            if (recepient == null)
+                return BadRequest("Could not Find User");
+
+            var messageToSend = _mapper.Map<Message>(message);
+
+            _repo.Add(messageToSend);
+
+            if (await _repo.SaveAll())
+            {
+                var messageToReturn = _mapper.Map<MessageToReturnDto>(messageToSend);
+
+                return CreatedAtRoute("GetMessage", new { id = messageToSend.Id }, messageToReturn);
+            }
+
+            throw new Exception("Sending Message failed on Sending");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery]MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageParams.UserId = userId;
+
+            var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+
+            return Ok(messages);
+
+        }
+
+        [HttpGet("thread/{recepientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recepientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messagesFromRepo = await _repo.GetMessageThread(userId, recepientId);
+
+            var messageThread = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+
+            return Ok(messageThread);
+        }
+
+        [HttpPost("{id}")]
+        public async Task<IActionResult> DeleteMessage(int id, int userId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await _repo.GetMessage(id);
+
+            if (messageFromRepo.SenderId == userId)
+                messageFromRepo.SenderDeleted = true;
+
+            if (messageFromRepo.RecepientId == userId)
+                messageFromRepo.RecepientDeleted = true;
+
+            if (messageFromRepo.RecepientDeleted && messageFromRepo.SenderId == userId)
+                _repo.Delete(messageFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception("Error Deleting message");
+        }
+
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var message = await _repo.GetMessage(id);
+
+            if (message.RecepientId != userId)
+                return Unauthorized();
+
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+
+
+
+        }
+
+    }
+}
---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,171 @@
+﻿using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IDatingRepository _datingRepo;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IDatingRepository datingRepo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            _datingRepo = datingRepo;
+            _mapper = mapper;
+            _cloudinaryConfig = cloudinaryConfig;
+
+            Account acc = new Account
+            (
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
+
+
+        }
+
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await _datingRepo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, [FromForm]PhotoForCreationDto photoForCreation)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _datingRepo.GetUser(userId);
+
+            var file = photoForCreation.File;
+
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoForCreation.Url = uploadResult.Uri.ToString();
+            photoForCreation.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreation);
+
+            if (!userFromRepo.Photos.Any(x => x.IsMain))
+                photo.IsMain = true;
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await _datingRepo.SaveAll())
+            {
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+
+                return CreatedAtRoute("GetPhoto", new { id = photo.Id }, photoToReturn);
+            }
+
+            return BadRequest("Could not add Photo");
+        }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _datingRepo.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _datingRepo.GetPhoto(id);
+
+            if (photoFromRepo.IsMain == true)
+                return BadRequest("This is alreday the main photo");
+
+            var currentMainPhoto = await _datingRepo.GetMainPhotoForUser(userId);
+
+            currentMainPhoto.IsMain = false;
+            photoFromRepo.IsMain = true;
+
+            if (await _datingRepo.SaveAll())
+                return NoContent();
+
+            return BadRequest("Could not set photo to main");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _datingRepo.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _datingRepo.GetPhoto(id);
+
+            if (photoFromRepo.IsMain == true)
+                return BadRequest("You Cannot Delete your main Photo");
+
+            if (photoFromRepo.PublicId != null)
+            {
+                var deleteParams = new DeletionParams(photoFromRepo.PublicId);
+
+                var result = _cloudinary.Destroy(deleteParams);
+
+                if (result.Result == "ok")
+                {
+                    _datingRepo.Delete(photoFromRepo);
+                }
+            }
+
+            if (photoFromRepo.PublicId == null)
+            {
+                _datingRepo.Delete(photoFromRepo);
+            }
+
+            if (await _datingRepo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to delete the Photo");
+        }
+
+    }
+}
